048b477c3e1d4ced4a5122db45a6edaadf0ccf39
duck967 <duck967@gmail.com>
2022-03-02 22:40:03 +0000

    create add todo component. add uuids. npm i uuid

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,23 +1,26 @@
 import React from "react";
 
+import { v4 as uuidv4 } from "uuid";
+
 import TodosList from "./TodosList";
 import Header from "./Header";
+import InputTodo from "./InputTodo"
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false
       }
@@ -48,10 +51,22 @@ class TodoContainer extends React.Component {
     });
   };
 
+  addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList 
           todos={this.state.todos} 
           handleChangeProps={this.handleChange} 
