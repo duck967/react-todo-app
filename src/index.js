2f449d0814477216ede788c83a6c592773f4b618
duck967 <duck967@gmail.com>
2022-03-04 18:00:33 +0000

    convert classes to functions and use state hooks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./functionBased/components/TodoContainer"
 
 
 //stylesheet
-import "./App.css"
+import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
